eba2a458b0836adb06458304c96797c7101d21d9
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-11-15 00:40:31 +0300

    added.

---
+++ JAVASCRIPT/Fundamentals/Variables, Data Types/app.js
@@ -0,0 +1,38 @@
+// Değişken Oluşturma
+
+// var a = 20;
+// var b = 10;
+// var c = 40;
+
+// console.log(a, b, c);
+
+// Primitive
+
+
+// Number
+
+// var a = 10; // number veri tipi
+// var b = 3.14; // number veri tipi
+// console.log(typeof a);
+// console.log(typeof b);
+
+
+
+// String
+
+// var name = "Mustafa";
+
+// console.log(name);
+// console.log(typeof name);
+
+
+
+// Boolean
+
+// var a = true;
+// console.log(typeof a);
+
+
+
+
+// Reference Veri Tipleri
