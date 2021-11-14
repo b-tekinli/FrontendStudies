a47d7d9b08f90b1444e5405725164eaa63c33987
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-11-15 00:01:23 +0300

    added.

---
+++ JAVASCRIPT/Fundamentals/Console/app.js
@@ -0,0 +1,20 @@
+console.log("Merhaba Javascript");
+console.log("321232121");
+console.log(true);
+console.log(false);
+
+
+var a = 20;
+console.log(a);
+console.log(typeof a);
+
+
+console.log(["Ahmet", "Mehmet"]);
+
+
+console.warn("Bu bir uyarıdır!");
+console.error("Bu sayfa bulunamadı!");
+
+console.clear();
+
+
