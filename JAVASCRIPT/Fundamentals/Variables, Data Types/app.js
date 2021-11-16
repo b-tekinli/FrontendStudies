64e35701dd214c8b242578b01542761dc0b83799
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-11-16 04:24:20 +0300

    added.

--- JAVASCRIPT/Fundamentals/Variables, Data Types/app.js
+++ JAVASCRIPT/Fundamentals/Variables, Data Types/app.js
@@ -20,7 +20,7 @@
 
 // String
 
-// var name = "Mustafa";
+// var name = "Mehmet";
 
 // console.log(name);
 // console.log(typeof name);
@@ -33,6 +33,61 @@
 // console.log(typeof a);
 
 
+// var a = null;
+// console.log(a);
+
+// console.log(typeof a);
+
+
+// var a;
+// console.log(a);
+
+
+
 
 
 // Reference Veri Tipleri
+
+// var numbers = [1, 2, 3, 4, 5];
+// console.log(numbers);
+// console.log(typeof numbers);
+// console.log(numbers[0]);
+
+
+// var person = {
+//     name = "Beyza Tekinli",
+//     age = 25
+// }
+
+// console.log(person);
+// console.log(typeof person);
+
+
+// var date = new Date();
+// console.log(date);
+// console.log(typeof date);
+
+
+// var merhaba = function() {
+//     console.log("Merhaba");
+// }
+
+// console.log(merhaba);
+// console.log(typeof merhaba);
+
+
+
+
+// var a = 10;
+// var b = a;
+// console.log(a, b);
+
+// a = 20;
+// console.log(a, b);
+
+
+
+// var a = [1, 2, 3];
+// var b = a;
+// a.push(4);
+// console.log(b);
