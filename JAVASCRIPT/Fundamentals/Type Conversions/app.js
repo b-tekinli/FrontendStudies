e5dba8ce67fbd1db0043ce045a6bb5fc62a71ac3
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-11-17 00:41:42 +0300

    added.

--- JAVASCRIPT/Fundamentals/Type Conversions/app.js
+++ JAVASCRIPT/Fundamentals/Type Conversions/app.js
@@ -2,34 +2,34 @@ let value;
 
 // Veritiplerini String'e Çevirme
 
-value = String(123);
-value = String(3.14);
-value = String(true);
-value = String(false);
-value = String(function () {
-    console.log()
-});
-value = String([1, 2, 3, 4]);
+// value = String(123);
+// value = String(3.14);
+// value = String(true);
+// value = String(false);
+// value = String(function () {
+//     console.log()
+// });
+// value = String([1, 2, 3, 4]);
 
-value = (10).toString();
-value = (3.14).toString();
+// value = (10).toString();
+// value = (3.14).toString();
 
 
 // Veritiplerini Sayılara Çevirme
 
-value = Number("123");
-value = Number(null);
-value = Number(undefined);
-value = Number("Hello World");
-value = Number(function() {
-    console.log()
-});
-value = Number([1, 2, 3, 4]);
+// value = Number("123");
+// value = Number(null);
+// value = Number(undefined);
+// value = Number("Hello World");
+// value = Number(function() {
+//     console.log()
+// });
+// value = Number([1, 2, 3, 4]);
 
-value = Number("3.14");
-value = parseFloat("3.14");
+// value = Number("3.14");
+// value = parseFloat("3.14");
 
-value = parseInt("3");
+// value = parseInt("3");
 
 
 
