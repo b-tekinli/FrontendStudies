7c1af77c41642efc02091854727efeb2e180b0d8
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-11-17 05:13:08 +0300

    added.

--- JAVASCRIPT/Fundamentals/Window Object/app.js
+++ JAVASCRIPT/Fundamentals/Window Object/app.js
@@ -7,7 +7,52 @@ alert("Merhaba");
 const cevap = confirm("Emin misiniz?");
 if (cevap) {
     console.log("Silin gitsin.");
-}
-else {
+} else {
     console.log("Silmeyin.");
 }
+
+
+
+// Prompt
+const cevap = prompt("2 + 2 = ?");
+
+if (cevap == "4") {
+    console.log("Doğru");
+} else {
+    console.log("Yanlış");
+}
+
+
+
+// Location
+let value;
+
+value = window;
+value = window.location;
+value = window.location.host;
+value = window.location.hostname;
+value = window.location.port;
+value = window.location.href;
+
+
+
+if (confirm("Sayfa yenilensin mi?")) {
+    window.location.reload();
+} else {
+    console.log("Sayfa yenilenmedi.");
+}
+
+
+value = window.outerHeight;
+value = window.outerWidth;
+
+
+value = value.innerHeight;
+value = value.innerWidth;
+
+
+value = window.scrollX;
+value = window.scrollY;
+
+
+console.log(value);
