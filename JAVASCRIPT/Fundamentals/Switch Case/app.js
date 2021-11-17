d06388ef0493a0a03633fdfd98c785e35ab7a48a
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-11-17 04:08:12 +0300

    added.

---
+++ JAVASCRIPT/Fundamentals/Switch Case/app.js
@@ -0,0 +1,16 @@
+const process = 2;
+
+switch (process) {
+    case 1:
+        console.log("İşlem 1");
+        break;
+    case 2:
+        console.log("İşlem 2");
+        break;
+    case 3:
+        console.log("İşlem 3");
+        break;
+    default:
+        console.log("Geçersiz işlem...");
+        break;
+}
