a8734740368c4d84ea13ab1ae4581aa062d6956e
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-11-18 20:15:11 +0300

    added.

---
+++ JAVASCRIPT/Dom Manipulation/Element Secme Yontemleri - 2/app.js
@@ -0,0 +1,9 @@
+const element = document.querySelector("#clear-todos");
+
+
+// Element Özellikleri
+
+console.log(element.id);
+console.log(element.className);
+console.log(element.classList);
+console.log(element.classList)[1];
