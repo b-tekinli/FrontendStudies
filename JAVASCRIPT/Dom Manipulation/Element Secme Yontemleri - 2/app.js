fab2f1a4b582760302f79a3b1cf298c91fae9954
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-11-29 00:13:37 +0300

    updated.

--- JAVASCRIPT/Dom Manipulation/Element Secme Yontemleri - 2/app.js
+++ JAVASCRIPT/Dom Manipulation/Element Secme Yontemleri - 2/app.js
@@ -16,6 +16,30 @@ console.log(element.style);
 // Style ve Element Özelliklerini Değiştirme
 element.className = "btn btn-success";
 element.style.color = "#000";
+element.style.marginLeft = "50px";
+element.href = "https://wwww.google.com.tr";
+element.target = "_blank";
+element.textContent = "Silin";
+element.innerHTML = "<span style = 'color:green'>Silin</span>"
+
+let element2 = document.querySelector("li:last-child");
+element2 = document.querySelector("li:nth-child(2)");
+element2 = document.querySelector("li:nth-child(3)");
+element2 = document.querySelector("li:nth-child(4)");
+element2 = document.querySelectorAll("li:nth-child(odd)");
+element2 = document.querySelectorAll("li:nth-child(even)");
+
+element2.forEach(function(el){
+    el.style.background = "#ccc";
+    el.style.color = "red";
+})
+
+const elements = document.querySelectorAll(".list-group-item"); // Node List
+elements.forEach(function(el) {
+    el.style.color = "red";
+    el.style.background = "eee";
+})
 
 
+// console.log(element.textContent);
 // console.log(element);
