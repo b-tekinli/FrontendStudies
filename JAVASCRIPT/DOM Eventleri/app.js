95745e4c93dde1b2be737c81a78827fa8fc989c8
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-12-02 02:39:21 +0300

    added.

---
+++ JAVASCRIPT/DOM Eventleri/app.js
@@ -0,0 +1,28 @@
+const filterInput = document.getElementById("filter");
+const todoForm  = document.getElementById("todo-form");
+
+todoForm.addEventListener("submit", submitForm);
+
+function submitForm(e) {
+    console.log("Submit Eventi");
+
+    e.preventDefault();
+}
+
+
+filterInput.addEventListener("focus", function(e){
+    console.log(e);
+    console.log(e.type);
+    console.log(e.target);
+    console.log(e.target.placeholder);
+    console.log(e.target.classname);
+
+
+    console.log("Naber");
+
+});
+
+
+filterInput.onfocus = function() {
+    console.log("Naber");
+}
