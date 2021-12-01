41e468178174479dfb4a0bb2e395e67edfcefc1f
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-12-01 20:16:37 +0300

    added.

---
+++ JAVASCRIPT/Dom Manipulation/Dinamik Özellik Değiştirme/app.js
@@ -0,0 +1,19 @@
+// REPLACE 
+
+const cardbody = document.querySelectorAll(".card-body")[1];
+
+const newElement = document.createElement("h3");
+
+newElement.className = "card-title";
+newElement.id = "tasks-title";
+newElement.textContent = "Yeni Todolar";
+
+
+// Eski Element
+
+const oldElement = document.querySelector("#tasks-title");
+cardbody.replaceChild(newElement, oldElement);
+
+
+console.log(cardbody);
+console.log(newElement);
