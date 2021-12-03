52831c10dfbdfcef4df3c8503dd7c5663446dabd
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-12-03 23:04:01 +0300

    added.

---
+++ JAVASCRIPT/DOM Eventleri/Keyboard Eventleri/app.js
@@ -0,0 +1,31 @@
+// Klavye Eventleri
+
+
+
+// keypress
+document.addEventListener("keypress", run);
+
+function run(e) {
+    // console.log(e.which);
+    console.log(e.key);
+}
+
+
+
+// keydown
+document.addEventListener("keydown", run);
+
+function run(e) {
+    // console.log(e.which);
+    console.log(e.key);
+}
+
+
+
+// keyup
+document.addEventListener("keyup", run);
+
+function run(e) {
+    // console.log(e.which);
+    console.log(e.key);
+}
