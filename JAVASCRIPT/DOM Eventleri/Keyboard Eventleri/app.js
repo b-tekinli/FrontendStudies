c11f8710f38d7e0525afec3a372dd9aa437924e0
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-12-03 23:06:58 +0300

    added.

--- JAVASCRIPT/DOM Eventleri/Keyboard Eventleri/app.js
+++ JAVASCRIPT/DOM Eventleri/Keyboard Eventleri/app.js
@@ -1,5 +1,13 @@
 // Klavye Eventleri
+const header = document.querySelector(".card-header");
+const todoInput = document.querySelector("#todo");
 
+todoInput.addEventListener("keyup", changeText);
+
+function changeText(e) {
+    header.textContent = e.target.value;
+    console.log(e.target.value);
+}
 
 
 // keypress
