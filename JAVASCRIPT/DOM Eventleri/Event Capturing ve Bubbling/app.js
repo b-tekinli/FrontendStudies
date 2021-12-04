de8bcbdbea26d3ec63fbdf912dc4ef1a87af7d23
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-12-04 19:53:35 +0300

    added.

---
+++ JAVASCRIPT/DOM Eventleri/Event Capturing ve Bubbling/app.js
@@ -0,0 +1,33 @@
+// Event Bubbling
+document.querySelector(".container").addEventListener("click", function(){
+    console.log("Div Container");
+});
+
+
+document.querySelector(".card.row").addEventListener("click", function(){
+    console.log("Card Row");
+});
+
+
+document.querySelectorAll(".card-body")[1].addEventListener("click", function(){
+    console.log("Card Body");
+});
+
+
+// Event Capturing / Delegation
+const cardBody = document.querySelectorAll(".card-body")[1];
+cardBody.addEventListener("click", run);
+
+function run(e) {
+    if (e.target.className === "fa fa-remove") {
+        console.log("Silme İşlemi");
+    }
+    if (e.target.id === "filter") {
+        console.log("Filtreleme İşlemi");
+    }
+    if (e.target.id === "clear-todos") {
+        console.log("Tüm taskları silme işlemi");
+    }
+    
+    // console.log(e.target);
+}
