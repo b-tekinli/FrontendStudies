a879111d50a456981b1229165e5870bf43793111
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-12-04 21:04:03 +0300

    added.

---
+++ JAVASCRIPT/Depolama Alanları/Local Storage/app.js
@@ -0,0 +1,66 @@
+// Local Storage
+
+// SetItem
+localStorage.setItem("hareket", "burpee");
+localStorage.setItem("tekrar", 50);
+
+
+// GetItem
+const value = localStorage.getItem("tekrar");
+console.log(value);
+console.log(typeof value);
+
+
+// Clear Local Storage
+localStorage.setItem("hareket", "burpee");
+localStorage.setItem("tekrar", 50);
+
+console.log(localStorage.getItem("sport"));
+
+if (localStorage.getItem("sport") == null) {
+    console.log("Sorguladığınız veri bulunmuyor!");
+}
+else {
+    console.log("Sorguladığnınz veri bulunuyor.");
+}
+
+// localStorage.clear();
+
+
+// Local Storage - Array Yazma
+const todos = ["Todo 1", "Todo 2", "Todo 3"];
+
+localStorage.setItem("todos", JSON.stringify(todos));
+
+const value = JSON.parse(localStorage.getItem("todos"));
+console.log(value);
+
+
+
+const form = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo");
+
+
+form.addEventListener("submit", addTodo);
+
+function addTodo(e) {
+    const value = todoInput.value;
+
+    let todos;
+
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    }
+    else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    todos.push(value);
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+
+
+    e.preventDefault();
+}
+
